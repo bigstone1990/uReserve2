cf6d9d71ff74b82b13a34389922bf88a9b0ecacb
bigstone1990 <masatakalman@gmail.com>
2024-12-07 13:59:48 +0900

    カレンダー

---
+++ resources/js/Components/CalendarDay.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { Link } from '@inertiajs/vue3';
+
+defineProps({
+  day: String,
+  day_name: String,
+  events: Object
+});
+</script>
+
+<template>
+  <div>
+    <div class="py-1 px-2 border border-gray-200 text-center">{{ day }}</div>
+    <div class="py-1 px-2 border border-gray-200 text-center">{{ day_name }}</div>
+    <div v-for="event in events" :key="event.index" :class="{'bg-green-400': event.reserved}" class="py-1 px-2 h-8 border border-gray-200">
+      <!-- <Link v-if="event.id" :href="route('events.detail', {id: event.id})">{{ event.name }}</Link> -->
+      {{ event.name }}
+    </div>
+  </div>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use Inertia\Inertia;
 use App\Http\Controllers\EventController;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
+    return Inertia::render('Calendar', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
         'laravelVersion' => Application::VERSION,
